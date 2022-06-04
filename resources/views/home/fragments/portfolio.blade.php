b6de9de9df47f676da13b715a35af8fb0c8e4afb
Eazygee <82817023+Eazygee@users.noreply.github.com>
2022-06-04 23:20:57 +0100

    Fixes

--- resources/views/home/fragments/portfolio.blade.php
+++ resources/views/home/fragments/portfolio.blade.php
@@ -16,71 +16,71 @@
         <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
 
             <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                <div class="portfolio-img"><img src="assets/img/portfolio/lap2.jpg" class="img-fluid" alt=""></div>
+                <div class="portfolio-img"><img src="{{ asset('assets/img/portfolio/lap2.jpg') }}" class="img-fluid" alt=""></div>
                 <div class="portfolio-info">
                     <h4>App 1</h4>
                     <p>App</p>
-                    <a href="assets/img/portfolio/lap2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
+                    <a href="{{ asset('assets/img/portfolio/lap2.jpg') }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
                     <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                <div class="portfolio-img"><img src="assets/img/portfolio/lap3.jpg" class="img-fluid" alt=""></div>
+                <div class="portfolio-img"><img src="{{ asset('assets/img/portfolio/lap3.jpg') }}" class="img-fluid" alt=""></div>
                 <div class="portfolio-info">
                     <h4>Web 3</h4>
                     <p>Web</p>
-                    <a href="assets/img/portfolio/lap3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
+                    <a href="{{ asset('assets/img/portfolio/lap3.jpg') }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
                     <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                <div class="portfolio-img"><img src="assets/img/portfolio/lap4.jpg" class="img-fluid" alt=""></div>
+                <div class="portfolio-img"><img src="{{ asset('assets/img/portfolio/lap4.jpg') }}" class="img-fluid" alt=""></div>
                 <div class="portfolio-info">
                     <h4>App 2</h4>
                     <p>App</p>
-                    <a href="assets/img/portfolio/lap4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
+                    <a href="{{ asset('assets/img/portfolio/lap4.jpg') }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
                     <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                <div class="portfolio-img"><img src="assets/img/portfolio/ps2.jpg" class="img-fluid" alt=""></div>
+                <div class="portfolio-img"><img src="{{ asset('assets/img/portfolio/ps2.jpg') }}" class="img-fluid" alt=""></div>
                 <div class="portfolio-info">
                     <h4>Card 2</h4>
                     <p>Card</p>
-                    <a href="assets/img/portfolio/ps2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
+                    <a href="{{ asset('assets/img/portfolio/ps2.jpg') }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
                     <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                <div class="portfolio-img"><img src="assets/img/portfolio/phone1.jpg" class="img-fluid" alt=""></div>
+                <div class="portfolio-img"><img src="{{ asset('assets/img/portfolio/phone1.jpg') }}" class="img-fluid" alt=""></div>
                 <div class="portfolio-info">
                     <h4>Web 2</h4>
                     <p>Web</p>
-                    <a href="assets/img/portfolio/phone1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
+                    <a href="{{ asset('assets/img/portfolio/phone1.jpg') }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
                     <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt=""></div>
+                <div class="portfolio-img"><img src="{{ asset('assets/img/portfolio/portfolio-6.jpg') }}" class="img-fluid" alt=""></div>
                 <div class="portfolio-info">
                     <h4>App 3</h4>
                     <p>App</p>
-                    <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 3"><i class="bx bx-plus"></i></a>
+                    <a href="{{ asset('assets/img/portfolio/portfolio-6.jpg') }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 3"><i class="bx bx-plus"></i></a>
                     <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt=""></div>
+                <div class="portfolio-img"><img src="{{ asset('assets/img/portfolio/portfolio-7.jpg') }}" class="img-fluid" alt=""></div>
                 <div class="portfolio-info">
                     <h4>Card 1</h4>
                     <p>Card</p>
-                    <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
+                    <a href="{{ asset('assets/img/portfolio/portfolio-7.jpg') }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
                     <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
@@ -96,11 +96,11 @@
             </div> -->
 
             <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                <div class="portfolio-img"><img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt=""></div>
+                <div class="portfolio-img"><img src="{{ asset('assets/img/portfolio/portfolio-9.jpg') }}" class="img-fluid" alt=""></div>
                 <div class="portfolio-info">
                     <h4>Web 3</h4>
                     <p>Web</p>
-                    <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
+                    <a href="{{ asset('assets/img/portfolio/portfolio-9.jpg') }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
                     <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
